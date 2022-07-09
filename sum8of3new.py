b9ecd429c5335816ef7ba7ecff0f58b8d68cea0b
MichaelMeier2907 <100663243+MichaelMeier2907@users.noreply.github.com>
2022-07-09 07:48:57 +0200

    dimod
    obj = dimod.BinaryQuadraticModel()

--- sum8of3new.py
+++ sum8of3new.py
@@ -6,7 +6,7 @@
 import dimod
 from dwave.system import EmbeddingComposite, DWaveSampler
 # define obj as quadratic model 
-obj = BinaryQuadraticModel()
+obj = dimod.BinaryQuadraticModel()
 # QUBO a0 = -20, a1 = -33, a2 = -44, a3 = - 53, a4 = -60, b01 = 6, b02 = 8, b03 = 10, b04 = 12, b12 = 14, b13 = 18, b14 = 22, b23 = 26, b24 = 32, b34 = 42
 # Q = {(0,0):-20,(1,1):-33,(2, 2):-44,(3,3):-53,(4,4):-60,(0,1):6,(0,2):8,(0,3):10,(0,4):12,(1,2):14,(1,3):18,(1,4):22,(2,3):26,(2,4):32,(3,4):42}
 for i in range (5):
